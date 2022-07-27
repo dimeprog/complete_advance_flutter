8f5cc316bd5549a96dd1265226a6feb92201284e
dimeprog <dimejiawoyefa5@gmail.com>
2022-07-27 22:17:30 +0100

    çompleted the leature 1

---
+++ complete_advance_flutter/lib/test.dart
@@ -0,0 +1,20 @@
+import 'package:complete_advance_flutter/app/app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+  void updataState() {
+    MyApp.instance.appState = 10;
+  }
+
+  void printState() {
+    print(MyApp.instance.appState);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

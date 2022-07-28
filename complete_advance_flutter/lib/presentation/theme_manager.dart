96823e81e1a3d6c38174a89980279fcb7b136230
dimeprog <dimejiawoyefa5@gmail.com>
2022-07-28 16:26:17 +0100

    completed appbar theme, button theme, card theme

--- complete_advance_flutter/lib/presentation/theme_manager.dart
+++ complete_advance_flutter/lib/presentation/theme_manager.dart
@@ -1,17 +1,59 @@
 import 'package:complete_advance_flutter/presentation/color_manager.dart';
+import 'package:complete_advance_flutter/presentation/font_manager.dart';
+import 'package:complete_advance_flutter/presentation/style_manager.dart';
+import 'package:complete_advance_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main color of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.darkGrey,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
-    accentColor: ColorManager.grey,
-    // card view  theme
-    //  app bar view theme
-    // text theme
-    // input decoration( text filed them)
-  );
+      // main color of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.darkGrey,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
+      accentColor: ColorManager.grey,
+      // card view  theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.grey,
+      ),
+      // ripple
+      shadowColor: ColorManager.primaryOpacity70,
+      //  button view theme
+      buttonTheme: ButtonThemeData(
+        buttonColor: ColorManager.primary,
+        disabledColor: ColorManager.grey1,
+        shape: StadiumBorder(),
+        splashColor: ColorManager.primaryOpacity70,
+      ),
+      //  elevated button
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            primary: ColorManager.primary,
+            elevation: AppSize.s4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                AppSize.s12,
+              ),
+            ),
+            textStyle: getRegularTextStyle(
+              color: ColorManager.white,
+              fontSize: FontSizeManager.s12,
+            )),
+      ),
+      //  app bar view theme
+      appBarTheme: AppBarTheme(
+        elevation: AppSize.s4,
+        color: ColorManager.primary,
+        centerTitle: true,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularTextStyle(
+          color: ColorManager.white,
+          fontSize: FontSizeManager.s16,
+        ),
+      )
+      // text theme
+      // input decoration( text filed them)
+      );
 }
--- complete_advance_flutter/lib/presentation/values_manager.dart
+++ complete_advance_flutter/lib/presentation/values_manager.dart
@@ -23,6 +23,7 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s4 = 4.0;
   static const double s10 = 10.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;

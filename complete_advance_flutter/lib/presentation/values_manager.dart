87a8467175172a5955d2de8822c69f3364ce95d6
dimeprog <dimejiawoyefa5@gmail.com>
2022-07-29 12:10:47 +0100

    completed theming the app

--- complete_advance_flutter/lib/presentation/values_manager.dart
+++ complete_advance_flutter/lib/presentation/values_manager.dart
@@ -11,6 +11,7 @@ class AppMargin {
 }
 
 class AppPadding {
+  static const double p8 = 8.0;
   static const double p10 = 10.0;
   static const double p12 = 12.0;
   static const double p14 = 14.0;
@@ -23,7 +24,9 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s1_5 = 1.5;
   static const double s4 = 4.0;
+  static const double s8 = 8.0;
   static const double s10 = 10.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;
